7ffa59230820dd488ef945b6dcbd4ce4f1b29e4a
danielld75 <danielld@poczta.fm>
2018-02-27 22:40:28 +0100

    First commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '618203737301-tilb6s1nfe27vdal7eut8h66po7lo650.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'buoLfhk2E-Zirl6fFDI_MTwA',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
